b4cb089153ca6b02b0f22bbef8bdc3c24a4729f7
Tzipora-Gugeniem <t0583243441@gmail.com>
2026-04-17 10:15:46 +0300

    Change baseURL to root for API requests

--- react/src/Project/api/api.js
+++ react/src/Project/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001', // הכתובת של שרת ה-Node.js שלך
+    baseURL: '/', // הכתובת של שרת ה-Node.js
     withCredentials: true,             // חובה! כדי שהעוגיות (Token) יעברו בין השרת ללקוח
 });
 
