de17546fca6e9a7e8af19e45353a489eaacd60f5
Tzipora-Gugeniem <t0583243441@gmail.com>
2026-04-22 15:46:22 +0300

    Update baseURL in API configuration
    Removed trailing slash from baseURL in API configuration.

--- react/src/Project/api/api.js
+++ react/src/Project/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api/', // הכתובת של שרת ה-Node.js
+    baseURL: '/api', // הכתובת של שרת ה-Node.js
     withCredentials: true,             // חובה! כדי שהעוגיות (Token) יעברו בין השרת ללקוח
 });
 
